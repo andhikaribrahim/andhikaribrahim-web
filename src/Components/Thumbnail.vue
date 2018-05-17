fab702d0208f00080cedfcf2640d59273889b8b3
Andhika R Ibrahim <andhikaribrahim@gmail.com>
2018-05-18 06:14:55 +0700

    Add thumbnail component & Add portfolio view

---
+++ src/Components/Thumbnail.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="card-deck">
+		<div class="card">
+			<img src="http://placehold.it/362x180" alt="Card image cap" class="card-img">
+			<div class="card-body">
+				<h5 class="card-title">Card Title</h5>
+				<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+			<div class="card-footer">
+				<small class="text-muted"><a href="#">Detail</a></small>
+			</div>
+		</div>
+	</div>
+</template>
---
+++ src/Views/Portfolio.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="projects container">
+		<div class="row d-flex justify-content-center">
+			<Thumbnail v-for="n in 6"></Thumbnail>
+		</div>
+	</div>
+</template>
+
+<script>
+	import Thumbnail from 'Components/Thumbnail';
+	export default {
+		components: {
+			Thumbnail
+		}
+	};
+</script>
