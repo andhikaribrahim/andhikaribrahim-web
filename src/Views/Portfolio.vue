efb36644fe819d7375278f2fa4fde4530172cac6
Andhika R Ibrahim <andhikaribrahim@gmail.com>
2018-05-19 16:39:27 +0700

    Add button component

--- src/Views/Portfolio.vue
+++ src/Views/Portfolio.vue
@@ -1,16 +1,23 @@
 <template>
-	<div class="projects container">
+	<div class="projects container text-center">
+		<h3 class="projects-title">Recent Work</h3>
 		<div class="row d-flex justify-content-center">
 			<Thumbnail v-for="n in 6"></Thumbnail>
 		</div>
+		<Button
+			btn-text="Load More.."
+			btn-style="btn btn-light"
+			class="projects-load"
+		></Button>
 	</div>
 </template>
 
 <script>
 	import Thumbnail from 'Components/Thumbnail';
+	import Button from 'Components/Button';
 	export default {
 		components: {
-			Thumbnail
+			Thumbnail, Button
 		}
 	};
 </script>
